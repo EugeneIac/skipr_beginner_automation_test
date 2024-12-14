199b367c7d32142fb74dad131049c40132f384fe
EugeneIac <neveray.md@gmail.com>
2024-12-14 19:16:56 +0200

    Initial commit

---
+++ src/tests/class.test.ts
@@ -0,0 +1,39 @@
+import { Class } from '../requests/Class';
+import fs from 'fs';
+import path from 'path';
+
+const getEnvValueForKey = (key: string) => {
+    const filePath = path.join(__dirname, '../../env.json');
+    const fileContent = fs.readFileSync(filePath, 'utf-8');
+    const parsedContent = JSON.parse(fileContent);
+    return parsedContent[key];
+};
+
+describe('Class test', () => {
+    let testClass: Class;
+    const baseUrl = 'https://jsonplaceholder.typicode.com';
+    const timeoutSec = 1;
+
+    beforeEach(() => {
+        testClass = new Class(baseUrl, timeoutSec);
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    describe('Tests', () => {
+        it('[TEST_1] should fetch post data successfully', async () => {
+            const response = await testClass.getPosts1();
+
+            expect(response.id).toEqual(getEnvValueForKey('EXPECTED_ID_TEST_1'));
+        });
+
+        it('[TEST_2] should fetch /posts/2 data successfully', async () => {
+            const response = await testClass.getPosts2();
+
+            // Verify the fetched data's ID matches the expected value in env.json
+            expect(response.id).toEqual(getEnvValueForKey('EXPECTED_ID_TEST_2'));
+        });
+    });
+});
---
+++ src/unit_tests/Class.test.ts
@@ -0,0 +1,64 @@
+import { Class } from '../requests/Class';
+import MockAdapter from 'axios-mock-adapter';
+
+describe('Class unit test', () => {
+    let testClass: Class;
+    let mock: MockAdapter;
+    const mockBaseUrl = 'https://example.com';
+    const mockTimeout = 5;
+
+    beforeEach(() => {
+        testClass = new Class(mockBaseUrl, mockTimeout);
+        mock = new MockAdapter(testClass['axiosClient']);
+    });
+
+    afterEach(() => {
+        mock.restore();
+    });
+
+    describe('getPosts1', () => {
+        it('should fetch post data successfully', async () => {
+            const mockPagesResponse = {
+                data: {
+                    "userId": 1,
+                    "id": 1,
+                    "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+                    "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+                }
+            };
+
+            mock.onGet('/posts/1').reply(200, mockPagesResponse);
+
+            const data = await testClass.getPosts1();
+            expect(data).toEqual(mockPagesResponse);
+        });
+
+        it('should throw an error if the getPages request fails', async () => {
+            mock.onGet('/api/pages').reply(500);
+
+            await expect(testClass.getPosts1()).rejects.toThrow();
+        });
+    });
+
+    describe('getPosts2', () => {
+        it('should fetch post data for /posts/2 successfully', async () => {
+            const mockPostsResponse = {
+                userId: 1,
+                id: 2,
+                title: 'qui est esse',
+                body: 'est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla'
+            };
+
+            mock.onGet('/posts/2').reply(200, mockPostsResponse);
+
+            const data = await testClass.getPosts2();
+            expect(data).toEqual(mockPostsResponse);
+        });
+
+        it('should throw an error if the getPosts2 request fails', async () => {
+            mock.onGet('/posts/2').reply(500);
+
+            await expect(testClass.getPosts2()).rejects.toThrow();
+        });
+    });
+});
